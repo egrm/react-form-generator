4c05fa3a333c96dde3e5e86a1d77c3ebc7828234
Egor <egorgromyko@gmail.com>
2019-07-15 16:00:40 +0200

    add config form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <h1>form generator</h1>;
+  const [config, setConfig] = React.useState(`{
+  "title": "my form",
+  "items": [
+    {
+      "label": "count",
+      "type": "number"
+    },
+    {
+      "label": "is editable",
+      "type": "checkbox"
+    },
+    {
+      "label": "caption",
+      "type": "textfield"
+    },
+    {
+      "label": "date",
+      "type": "datefield"
+    },
+    {
+      "label": "which color?",
+      "type": "radio"
+      "options": [
+        {
+          "label": "blue",
+          "value": "blue"
+        },
+        {
+          "label": "blue",
+          "value": "blue"
+        },
+        {
+          "label": "blue",
+          "value": "blue"
+        }
+      ]
+    },
+  ]
+}`);
+  return (
+    <>
+      <div id="config">
+        <form action="javascript:void(0)">
+          <textarea
+            name="config"
+            value={config}
+            onChange={(e) => {
+              setConfig(e.target.value);
+            }}
+          />
+          <br />
+          <button type="submit">Apply</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
