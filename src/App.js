b328b9e18ba04fbd8b36a11ae78ebc66e92842b4
Egor <egorgromyko@gmail.com>
2019-07-16 14:59:49 +0200

    add styles to center the app

--- src/App.js
+++ src/App.js
@@ -10,27 +10,50 @@ function App() {
   const [config, setConfig] = React.useState(testConfig);
   const tabClasses = "dn tab w-100 w-50-ns absolute";
   const tabNavClasses = {
-    forLabel: "",
-    forInput: "dn"
-  }
+    forLabel: "dib pa2 ma2 dim br2",
+    forInput: "dn",
+  };
   return (
-    <div className="helvetica">
-      <input type="radio" name="tabs" id="ConfigTab-nav" className={tabNavClasses.forInput} defaultChecked />
-      <label htmlFor="ConfigTab-nav" className={tabNavClasses.forLabel}>Config</label>
-      <input type="radio" name="tabs" id="ResultTab-nav" className={tabNavClasses.forInput} />
-      <label htmlFor="ResultTab-nav" className={tabNavClasses.forLabel}>Result</label>
-      <ConfigTab
-        onChange={(e) => {
-          setConfig(e.target.value);
-        }}
-        className={tabClasses}
-        value={config}
-      />
+    <div className="w-100 vh-100 flex flex-column justify-center items-center">
+      <div className="helvetica border-box w-100 w-50-ns vh-50-ns">
+        <input
+          type="radio"
+          name="tabs"
+          id="ConfigTab-nav"
+          className={tabNavClasses.forInput}
+          defaultChecked
+        />
+        <label
+          htmlFor="ConfigTab-nav"
+          className={tabNavClasses.forLabel}
+        >
+          Config
+        </label>
+        <input
+          type="radio"
+          name="tabs"
+          id="ResultTab-nav"
+          className={tabNavClasses.forInput}
+        />
+        <label
+          htmlFor="ResultTab-nav"
+          className={tabNavClasses.forLabel}
+        >
+          Result
+        </label>
+        <ConfigTab
+          onChange={(e) => {
+            setConfig(e.target.value);
+          }}
+          className={tabClasses}
+          value={config}
+        />
         <ResultTab
           config={JSON.parse(config)}
           className={tabClasses}
         />
-        </div>
+      </div>
+    </div>
   );
 }
 
