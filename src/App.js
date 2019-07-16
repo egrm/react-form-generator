13b3853c622ba7ca1b44c4c0210befde25c23792
Egor <egorgromyko@gmail.com>
2019-07-16 14:42:29 +0200

    add tab-switching css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import nanoid from "nanoid";
 import ResultTab from "./components/ResultTab/ResultTab.js";
 import ConfigTab from "./components/ConfigTab/ConfigTab.js";
 import "tachyons";
+import "./App.css";
 import testConfig from "./testConfig.js";
 
 function App() {
   const [config, setConfig] = React.useState(testConfig);
-  const tabClasses = "w-100 w-70-ns";
+  const tabClasses = "dn tab w-100 w-70-ns absolute";
+  const tabNavClasses = {
+    forLabel: "",
+    forInput: "dn"
+  }
   return (
     <>
+      <input type="radio" name="tabs" id="ConfigTab-nav" className={tabNavClasses.forInput} checked />
+      <label for="ConfigTab-nav" className={tabNavClasses.forLabel}>Config</label>
+      <input type="radio" name="tabs" id="ResultTab-nav" className={tabNavClasses.forInput} />
+      <label for="ResultTab-nav" className={tabNavClasses.forLabel}>Result</label>
       <ConfigTab
         onChange={(e) => {
           setConfig(e.target.value);
@@ -21,7 +30,7 @@ function App() {
         config={JSON.parse(config)}
         className={tabClasses}
       />
-    </>
+      </>
   );
 }
 
