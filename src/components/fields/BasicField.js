51d585f0f3c42f078687825f04d6804e87400678
Egor <egorgromyko@gmail.com>
2019-07-17 17:49:32 +0200

    add styles to BasicField

--- src/components/fields/BasicField.js
+++ src/components/fields/BasicField.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function BasicField({label, name, type}) {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input id={name} name={name} type={type} />
+    <div className="flex flex-row justify-between items-center w-100 mb2 pt2 pb2 bb b--light-gray">
+      <label htmlFor={name} className="pa1 mr1 self-end">
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className="w-100 mw5 pa1"
+        type={type}
+      />
     </div>
   );
 }
